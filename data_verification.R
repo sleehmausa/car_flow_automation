111c568865943a58c55ff44df04d5829d22f4cdc
Sangho Lee (Stan) <sanlee8495@gmail.com>
2025-02-08 13:38:06 -0800

    02082025

--- data_verification.R
+++ data_verification.R
@@ -4,3 +4,10 @@ final_normalized_data %>%
   filter(Subcategory == "Retail Sales") %>% 
   filter(str_detect(Month, "Q3")) 
 
+# Category Subcategory  Month   Value Model      Sheet_Name     
+# <chr>    <chr>        <chr>   <dbl> <chr>      <chr>          
+#   1 Sales    Retail Sales Q3...66     0 TUCSON HEV CF (Tucson HEV)
+# 2 Sales    Retail Sales Q3...71  6194 TUCSON HEV CF (Tucson HEV)
+# 3 Sales    Retail Sales Q3...75  6163 TUCSON HEV CF (Tucson HEV)
+# 4 Sales    Retail Sales Q3...79  7826 TUCSON HEV CF (Tucson HEV)
+# 5 Sales    Retail Sales Q3...83 15509 TUCSON HEV CF (Tucson HEV)
